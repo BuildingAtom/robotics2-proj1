2257812eae38c51eff63d7ae931189b710e8907e
Adam Li <84liadam@gmail.com>
2021-02-14 18:00:52 -0500

    untested

--- catkin_ws/src/drive_twist_middleman/drive_twist_middleman.py
+++ catkin_ws/src/drive_twist_middleman/drive_twist_middleman.py
@@ -1,31 +1,60 @@
 #!/usr/bin/env python
-# license here
+# todo: license here
+
+import random
 
 import rospy
-from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 
-def drive_twist_middleman():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+def callback_relay(in_msg):
+    global pub, forward_gaussian, turn_gaussian
+    
+    # the only input movements are going to be forwards/backwards velocity and angular rotation
+    # many controllers appear to use x for the forward direction, but the robot has y as the forward direction based on project spec
+    fwd = in_msg.linear.x
+    turn = in_msg.angular.z
+    
+    # generate noise if any nonzero input
+    if fwd or turn:
+        fwd_noise = random.gauss(forward_gaussian[0], forward_gaussian[1])
+        turn_noise = random.gauss(turn_gaussian[0], turn_gaussian[1])
+    
+    # create and publish the message with noise
+    msg = Twist()
+    msg.linear.y = fwd + fwd_noise
+    msg.angular.z = turn + turn_noise
+    
+    pub.publish(msg)
 
-if __name__ == '__main__':
-    global pub
+
+def drive_twist_middleman():
+    global pub, forward_gaussian, turn_gaussian
     
     # get the random noise
-    forward_gaussian = rospy.get_param('forward_gaussian', [0.0, 0.05])
-    turn_gaussian = rospy.get_param('turn_gaussian', [0.0, 0.05])
+    forward_gaussian = rospy.get_param('/simulation/forward_gaussian', [0.0, 0.05])
+    turn_gaussian = rospy.get_param('/simulation/turn_gaussian', [0.0, 0.05])
 
-    # get what to subscribe to and what to publish to
-    #rospy.get_param('
-    pub = rospy.Publisher('/robot/cmd_vel',
+    # get what to publish to and what to subscribe to
+    pubto = rospy.get_param('/simulation/drive_mm_out', '/robot/cmd_vel')
+    pub = rospy.Publisher(pubto, Twist, queue_size=10)
+    
+    subto = rospy.get_param('/simulation/drive_mm_in', '/controller/drive')
+    rospy.Subscriber(subto, Twist, callback_relay)
+
+    # store the parameters back so that the parameter server can be updated with default values if not present
+    rospy.set_param('/simulation/forward_gaussian', forward_gaussian)
+    rospy.set_param('/simulation/turn_gaussian', turn_gaussian)
+    rospy.set_param('/simulation/drive_mm_out', pubto)
+    rospy.set_param('/simulation/drive_mm_in', subto)
+    
+    # todo: consider adding way to change random noise on the fly
+    
+    #keep the process alive
+    rospy.spin()
+
+
+if __name__ == '__main__':
     try:
-        talker()
+        drive_twist_middleman()
     except rospy.ROSInterruptException:
         pass
---
+++ catkin_ws/src/proj1robot_description/scripts/generate_random_position.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# todo: license here
+
+import random
+import rospy
+
+from gazebo_msgs.msg import ModelState 
+from gazebo_msgs.srv import SetModelState
+
+from tf_conversions.transformations import quaternion_from_euler
+
+# mini helper function to generate the random starting position.
+
+def generate_random_position:
+    
+    # get the random noise and location
+    xyY_mean = rospy.get_param('/simulation/spawn_mean_gauss', [0.0, 2.0, -1.5708])
+    xyY_stddev = rospy.get_param('/simulation/spawn_stddev_gauss', [0.1, 0.1, 0.05])
+    rospy.set_param('/simulation/spawn_mean_gauss', xyY_mean)
+    rospy.set_param('/simulation/spawn_stddev_gauss', xyY_stddev)
+    
+    # generate values
+    xyY = []
+    for mean, stddev in zip(xyY_mean, xyY_stddev):
+        xyY.append(random.gauss(mean, stddev))
+    
+    print("-x " + str(xyY[0]) + " -y " + str(xyY[1]) + " -Y " + str(xyY[2]))
+    
+    q = quaternion_from_euler(0, 0, xyY[2])
+    
+    # generate the modelstate information
+    state = ModelState()
+    state.model_name = 'proj1robot'
+    state.pose.position.x = xyY[0]
+    state.pose.position.y = xyY[1]
+    state.pose.position.z = 0
+    state.pose.orientation.x = q[0]
+    state.pose.orientation.y = q[1]
+    state.pose.orientation.z = q[2]
+    state.pose.orientation.w = q[3]
+    
+    # now try to add it to the robot
+    print("waiting for gazeobo")
+    rospy.wait_for_service('/gazebo/set_model_state')
+    try:
+        set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+        set_model_state(state)
+        
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+    
+if __name__ == '__main__':
+    try:
+        generate_random_position()
+    except rospy.ROSInterruptException:
+        pass
